59df5bb7c23acda5cbbc257e7ede8f53f3d66081
Rafik Abdryakhimov <raaafik95@gmail.com>
2020-11-14 22:29:22 +0300

    Add method

--- GitInVS/GitInVS/Program.cs
+++ GitInVS/GitInVS/Program.cs
@@ -4,6 +4,11 @@ namespace GitInVS
 {
 	class Program
 	{
+		static void Print()
+		{
+			Console.WriteLine("print");
+		}
+		
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
